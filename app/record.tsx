2912e32260827a7a3de872a32ed0e7f2d8d03343
Kieran Paranjpe <kieranbellum@gmail.com>
2024-04-17 16:54:21 -0400

    final touches

--- app/record.tsx
+++ app/record.tsx
@@ -4,9 +4,16 @@ export default function Record({key, name, date, time} : {key: number, name: Str
 {
     return (
         <div className="flex items-center justify-between full h-15">
-            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>{date}</p>
+            <div style={{paddingInline: "1vw", flex: "1"}}>
+                <p className="text-left align-middle text-black">{date}</p>
+                <p className="text-left align-middle text-gray-500 text-sm" >{time}</p>
+
+            </div>
             <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "4"}}>{name}</p>
-            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>Download</p>
+
+            <div className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>
+                <img style={{marginLeft: "27px"}} width="25" height="25" src="https://img.icons8.com/ios/50/download--v1.png" alt="download--v1"/>
+            </div>
         </div>
     );
 }
