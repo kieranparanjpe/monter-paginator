967ba54d3e2cb18584134d64a0752b284d69aab3
Kieran Paranjpe <kieranbellum@gmail.com>
2024-04-17 16:42:40 -0400

    add row/page

--- app/Records.tsx
+++ app/Records.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {RecordDataType} from "@/app/RecordDataType";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Record from "@/app/record";
 
 export default function Records()
@@ -19,13 +19,60 @@ export default function Records()
             new RecordDataType("2024/04/04", "12:10 PM", "record11.csv"),
             new RecordDataType("2024/04/04", "16:03 PM", "record12.csv"),]
 
-    const [currentPage, setCurrentPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const[recordsToShow, setRecordsToShow] : [RecordDataType[], any] = useState([]);
+
+    const [currentPage, setCurrentPage] : [number, any] = useState(0);
+    const [rowsPerPage, setRowsPerPage] : [number, any] = useState(1);
+    useEffect(() => {
+        let start = currentPage * rowsPerPage;
+        let end = start + rowsPerPage;
+        setRecordsToShow(records.slice(start, end));
+
+    }, [currentPage, records, rowsPerPage]);
+
     return(
         <div>
-            {records.map((value, index)=>{
-                return <Record/>
+            {recordsToShow.map((value, index)=>{
+                return <Record key={index} date={value.getDate()} time={value.getTime()} name={value.getName()}/>
             })}
+            <div className="flex items-center justify-center h-20 space-x-3.5">
+                <div className={"flex items-center justify-center h-10 space-x-1.5"}>
+                    <button className={`w-15 h-10 text-black`}
+                            onClick={()=>{setCurrentPage(currentPage > 0 ? currentPage-1 : 0)}}>{"<<prev"}</button>
+                    <button style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                            className={`w-10 h-10 text-black ${currentPage==0? 'bg-orange-400' : 'bg-white'}`}
+                            onClick={()=>{setCurrentPage(0)}}>1</button>
+                    <button style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                            className={`w-10 h-10 text-black ${currentPage==1? 'bg-orange-400' : 'bg-white'}`}
+                            onClick={()=>{setCurrentPage(1)}}>2</button>
+                    <button style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                            className={`w-10 h-10 text-black ${currentPage==2? 'bg-orange-400' : 'bg-white'}`}
+                            onClick={()=>{setCurrentPage(2)}}>3</button>
+                    <button style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                            className={`w-10 h-10 text-black ${currentPage==3? 'bg-orange-400' : 'bg-white'}`}
+                            onClick={()=>{setCurrentPage(3)}}>4</button>
+                    <button style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                            className={`w-10 h-10 text-black ${currentPage==4? 'bg-orange-400' : 'bg-white'}`}
+                            onClick={()=>{setCurrentPage(4)}}>5</button>
+                    <button className={`w-15 h-10 text-black`}
+                            onClick={()=>{setCurrentPage(currentPage < 4 ? currentPage+1 : 4)}}>{"next>>"}</button>
+                </div>
+                <div className={"flex items-center justify-center h-10 space-x-1.5"}>
+                    <label className={"text-black"}>{"Rows per page    "}
+                        <select className={"w-14 text-center"} style={{borderRadius: "5px", borderWidth: "2px", borderColor:"black"}}
+                        onChange={(e)=>{setRowsPerPage(Number.parseInt(e.target.value))}}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                    </select></label>
+                </div>
+            </div>
         </div>
+
     )
 }
--- app/record.tsx
+++ app/record.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-export default function Record()
+export default function Record({key, name, date, time} : {key: number, name: String, date : String, time : String})
 {
     return (
-        <div className="flex items-center justify-between full h-10 bg-gray-300">
-            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>Date</p>
-            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "4"}}>Report Name</p>
+        <div className="flex items-center justify-between full h-15">
+            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>{date}</p>
+            <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "4"}}>{name}</p>
             <p className="text-left align-middle text-black" style={{paddingInline: "1vw", flex: "1"}}>Download</p>
         </div>
     );
